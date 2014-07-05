a6d07b2f6db9e80a8d7dd848d09cf55d78cb6f8d
haoxin <coderhaoxin@outlook.com>
2014-07-05 19:13:14 +0800

    xx

--- run.js
+++ run.js
@@ -1,122 +1,24 @@
 'use strict';
 
-var config = require('./config'),
+var cp = require('child_process'),
   path = require('path'),
-  debug = require('debug')('parse:run'),
-  co = require('co'),
-  dom = require('jsdom'),
-  csv = require('fast-csv');
+  execfile = path.resolve(__dirname, 'worker.js');
 
-var skip = process.argv[2] | 0,
-  limit = process.argv[3] | 0;
+var total = 65000,
+  interval = 200;
 
-var filepath = path.resolve(__dirname, config.file),
-  extname = path.extname(config.file);
-debug('filepath: %s, extname %s', filepath, extname);
+var i = 0;
 
-var csv = require('fast-csv');
+var start = Date.now();
 
-var origin = [];
-var result = [];
+setInterval(function() {
+  console.log('已进行: %n / %n', i, total);
+  var due = (Date.now() - start) / (60 * 1000);
+  console.log('耗时: %n min', due);
+  console.log('还需: %n min', due / i * (total - i));
+}, 10000);
 
-function getTitleFromUrl(url) {
-  return function(done) {
-    console.log('start:', url);
-
-    var t = setTimeout(function() {
-      var e = new Error('timeout');
-
-      console.log('timeout 3000');
-
-      done(e);
-    }, 3000);
-
-    dom.env(url, function(errors, window) {
-      if (errors) {
-        done(errors);
-      } else {
-        var title = '无标题';
-        try {
-          title = window.document.querySelector('title').text;
-        } catch (e) {
-          debug('error:', e);
-        }
-
-        clearTimeout(t);
-
-        console.log('get:', url);
-
-        done(null, title);
-      }
-    });
-  };
-}
-
-function write(data, i) {
-  return function(done) {
-    var dist = config.dist,
-      csvpath = dist.replace(/.csv$/, '-' + i + '.csv');
-
-    csv
-      .writeToPath(csvpath, data, {
-        headers: true
-      })
-      .on('finish', function() {
-        done();
-      });
-  };
-}
-
-csv
-  .fromPath(filepath)
-  .on('record', function(data) {
-    origin.push(data);
-  })
-  .on('end', function() {
-    console.log('parse csv done');
-
-    co(function * () {
-      yield getTitles();
-      yield writeToCsv();
-    })();
-  });
-
-
-function * getTitles() {
-  var url, data;
-
-  for (var i = 0; i < origin.length; i++) {
-    if (i < skip) {
-      continue;
-    }
-    if (i >= (skip + limit)) {
-      console.log(i, skip + limit, 'break');
-      break;
-    }
-
-    data = origin[i];
-
-    try {
-      url = data[8].split('=')[1];
-    } catch (e) {
-      debug('error:', e);
-    }
-
-    var title = '无标题';
-
-    try {
-      title = yield getTitleFromUrl(url);
-    } catch (e) {
-      debug('error:', e);
-    }
-
-    data.push(title.replace(/\n/g, ''));
-    result.push(data);
-
-    console.log(i);
-  }
-}
-
-function * writeToCsv() {
-  yield write(result, skip);
+for (i = 0; i < total; i += interval) {
+  var result = cp.spawnSync('node', ['--harmony', execfile, i, interval]);
+  console.log(i, result);
 }
