10d983c95ab94710d058d96036cfd297a8ed8e65
haoxin <coderhaoxin@outlook.com>
2014-07-23 20:39:46 +0800

    xx

---
+++ xxx/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var serve = require('koa-static');
+var koa = require('koa');
+var app = koa();
+
+app.use(serve('.'));
+
+app.listen(3000);
+
+console.log('listening on port 3000');
