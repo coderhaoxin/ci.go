023e4e36efa5694b86737a7c4ca8c159de5eb565
haoxin <coderhaoxin@outlook.com>
2014-07-24 15:56:19 +0800

    open api login

--- xxx/index.js
+++ xxx/index.js
@@ -4,6 +4,11 @@ var serve = require('koa-static');
 var koa = require('koa');
 var app = koa();
 
+app.use(function * (next) {
+  yield * next;
+  this.set('Access-Control-Allow-Origin', '*');
+});
+
 app.use(serve('.'));
 
 app.listen(3000);
